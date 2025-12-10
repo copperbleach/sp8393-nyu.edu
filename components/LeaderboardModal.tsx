8b03d2854059a2f4909c74850c1f402f1581e22c
copperbleach <sp8393@nyu.edu>
2025-12-10 01:53:30 -0500

    Update LeaderboardModal.tsx

--- components/LeaderboardModal.tsx
+++ components/LeaderboardModal.tsx
@@ -80,11 +80,6 @@ const LeaderboardModal: React.FC<LeaderboardModalProps> = ({ dayCount, onClose, 
         return leaderboard.some(e => e.username === userEntry.name && e.score === userEntry.score);
     }, [leaderboard, userEntry]);
     
-    const CopyIcon = ({id}: {id: string}) => (
-        copySuccessId === id ?
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-green-500" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 13l4 4L19 7" /></svg> :
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z" /></svg>
-    );
 
     return (
         <div className="fixed inset-0 bg-black/60 flex flex-col justify-center items-center z-50 p-4 font-sans animate-fade-in-down">
